401fda8a7e7669194e63c54bc1d6124f30a6989c
sasumaki <sausmakinen@gmail.com>
2025-04-10 12:24:01 +0300

    fix recommended

--- eslint-plugin-kysely-rules.js
+++ eslint-plugin-kysely-rules.js
@@ -4,22 +4,31 @@ const enforceNoAmbigiousColumnsRule = require("eslint-plugin-kysely/enforce-no-a
 const enforceKyselyOperatorsRule = require("eslint-plugin-kysely/enforce-kysely-operators");
 
 const plugin = {
+  meta: {
+    name: "kysely",
+  },
   rules: {
     "enforce-where-clause": enforceWhereClauseRule,
     "enforce-select-clause": enforceSelectClauseRule,
     "enforce-no-ambigious-columns": enforceNoAmbigiousColumnsRule,
     "enforce-kysely-operators": enforceKyselyOperatorsRule,
   },
-  configs: {
-    recommended: {
-      plugins: ["kysely"],
-      rules: {
-        "kysely/enforce-where-clause": "error",
-        "kysely/enforce-select-clause": "warn",
-        "kysely/enforce-no-ambigious-columns": "error",
-        "kysely/enforce-kysely-operators": "warn"
-      }
+  configs: {}
+};
+
+Object.assign(plugin.configs, {
+  recommended: {
+    plugins: {
+      kysely: plugin,
+    },
+    rules: {
+      "kysely/enforce-where-clause": "error",
+      "kysely/enforce-select-clause": "warn",
+      "kysely/enforce-no-ambigious-columns": "error",
+      "kysely/enforce-kysely-operators": "warn"
     }
   }
-};
+});
+
+// OR for CommonJS
 module.exports = plugin;
